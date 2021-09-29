9c76412a2f365bcf3cfd6c169e09694b520e4fcf
chezd <chetdilauro@gmail.com>
2021-09-29 10:17:55 -0700

    removed error periods

--- helloWorld.java
+++ helloWorld.java
@@ -1,6 +1,6 @@
-public.class HelloWorld{
+public class HelloWorld{
   
-  public.static void main(String[] args){
+  public static void main(String[] args){
     System.out.println("****** Hello World & Welcome to Jenkins Training ***");
   }
 }
