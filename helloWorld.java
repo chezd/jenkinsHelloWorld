61e6b03671fdce5e7584719a200b03462760a13e
chezd <chetdilauro@gmail.com>
2021-09-29 10:07:26 -0700

    quick fix

--- helloWorld.java
+++ helloWorld.java
@@ -1,5 +1,9 @@
 public.class HelloWorld{
-  public.static void main(String[]args){
-    System.out.println("****** Hello World & Welcome to Jenkins Training ***")
+  
+  public.static void main(String[] args){
+    System.out.println("****** Hello World & Welcome to Jenkins Training ***");
   }
 }
+
+
+\\
